47eae904d5f2e90d2c5a6441ee9ba0ed226f3f72
Paul Lindberg <paul.lindberg@mymail.champlain.edu>
2019-08-09 16:15:11 -0700

    Initialized pygame import and init files

---
+++ temperature/__init__.py
@@ -0,0 +1,14 @@
+import os
+from start import *
+
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+
+
+def main():
+    env = Environment()
+    while True:
+        env.menu()
+
+
+if __name__ == '__main__':
+    main()
---
+++ temperature/start.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+
+# PiTFT Screen Size (320x240)
+SCREEN_SIZE = 320, 240
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
